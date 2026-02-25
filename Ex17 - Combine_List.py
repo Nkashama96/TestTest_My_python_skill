f8320e555833b11001409c062154fa8e6688ba4a
Nkashama96 <147906315+Nkashama96@users.noreply.github.com>
2026-02-25 18:35:11 +0700

    Create Ex17 - Combine_List.py

---
+++ Ex17 - Combine_List.py
@@ -0,0 +1,16 @@
+'''
+Combine Lists Without Duplicates
+
+Problem Statement
+
+Write a function combine_lists(list1, list2) that returns a new list containing all elements from both input lists, but with no duplicate values.
+
+Requirements
+
+    The function should take two lists as arguments.
+
+    Return a new list with all elements from both lists, but no duplicates.
+
+    The order of elements should be preserved as much as possible.
+'''
+
