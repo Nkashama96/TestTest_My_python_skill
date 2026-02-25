ac362013a2276fd009a138d25bac4529d7b9fc50
Nkashama96 <147906315+Nkashama96@users.noreply.github.com>
2026-02-25 18:41:34 +0700

    Update Ex17 - Combine_List.py

--- Ex17 - Combine_List.py
+++ Ex17 - Combine_List.py
@@ -14,3 +14,23 @@ Requirements
     The order of elements should be preserved as much as possible.
 '''
 
+def combine_lists(list1, list2):
+    # TODO: Return a new list with all elements from both lists, no duplicates
+      combined_list = []
+      # Iterate through list1 and add elements if not already present
+      for item in list1:
+        if item not in combined_list:
+          combined_list.append(item)
+      
+      # Iterate through list2 and add elements if not already present
+      for item in list2:
+        if item not in combined_list:
+          combined_list.append(item)
+          
+      return combined_list
+      pass
+
+List1 = ['a','b','c','d','e','g']
+List2 = ['a','e','i','o','u']
+List3 = combine_lists(List1,List2)
+print(List3)
