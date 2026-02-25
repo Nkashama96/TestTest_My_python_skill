6caf44baf4fa9b2d672495df5b45fed332c85407
Nkashama96 <147906315+Nkashama96@users.noreply.github.com>
2026-02-25 17:59:15 +0700

    Create HelloWorld.py

---
+++ HelloWorld.py
@@ -0,0 +1,14 @@
+'''
+Hello, World!
+
+Using the print function in Python, print the string "Hello, World!"
+
+This is a classic beginner exercise to get started with Python functions and string return values.
+
+Requirements
+
+Print exactly the string "Hello, World!" (case and punctuation must match).
+'''
+
+# TODO: Print 'Hello, World!' to the console
+print ("Hello, World!");
