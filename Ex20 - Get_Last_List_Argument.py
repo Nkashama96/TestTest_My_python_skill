2ba1b707c27a3beff1c1d6f97e08ff8eae729814
Nkashama96 <147906315+Nkashama96@users.noreply.github.com>
2026-02-26 12:52:40 +0700

    Create Ex20 - Get_Last_List_Argument.py

---
+++ Ex20 - Get_Last_List_Argument.py
@@ -0,0 +1,12 @@
+'''
+Get Last List Argument
+
+Write a function get_last_list(*args) that returns the last list argument passed to it. If no lists are passed, return an empty list.
+
+Requirements
+
+    The function should accept any number of arguments.
+
+    Return the last argument that is a list, or an empty list if none are lists.
+'''
+
